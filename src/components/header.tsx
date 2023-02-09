ac30f419413eb84ac2375d1f31115ae38337857a
Ian Laidlaw <laidlaw434@gmail.com>
2023-02-08 19:17:21 -0600

    add state to the header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,40 @@
 import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import './header.css';
+
+type Page = {
+    route: string,
+    text: string,
+};
 
 export function Header() {
+    const [currentRoute, setCurrentRoute] = useState<string>('/');
+
+    const pages = [
+        { route: '/', text: 'Home' },
+        { route: '/slippi-replays', text: 'Slippi' },
+    ];
+
+    function renderLink({ route, text }: Page) {
+        const isActive = route === currentRoute;
+
+        return (
+            <Link 
+                key={`route-${route}`} 
+                className={isActive ?'disabled' : ''} 
+                onClick={() => setCurrentRoute(route)} 
+                to="/"
+            >
+                {text}
+            </Link>
+        )
+    }
+
+    console.log({currentRoute});
+
     return (
         <div>
-            <Link to="/">Home</Link>
-            <Link to="/slippi-replays">slippi</Link>
+            { pages.map(renderLink) }
         </div>
     );
 }
